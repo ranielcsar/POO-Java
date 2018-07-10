0db74c7f1b63fc5b0c881acfd76faaebdab16688
Raniel <whyfck@gmail.com>
2018-07-10 19:27:35 -0300

    Update ContaCorrente.java

--- Banco/ContaCorrente.java
+++ Banco/ContaCorrente.java
@@ -1,7 +1,7 @@
 public class ContaCorrente extends Conta {
    private int saqueMensal;
    private int extratoMensal;
-   private double limite;
+   private double limiteAtual;
    private double limiteInicial;
    
    public ContaCorrente(int conta, String agencia, String titular, String senha, double saldo)
@@ -10,8 +10,8 @@ public class ContaCorrente extends Conta {
       
       this.saqueMensal = saqueMensal;
       this.extratoMensal = extratoMensal;
-      this.limite = limite;
-      this.limiteInicial = limite;
+      this.limiteAtual = limiteAtual;
+      this.limiteInicial = limiteAtual;
    }
    
    public void debitoCesta(double valorDebito)
@@ -26,7 +26,7 @@ public class ContaCorrente extends Conta {
    
    public String sacar(double valorSaque)
    {
-      double saldoTotal = this.limite + getSaldo();
+      double saldoTotal = this.limiteAtual + getSaldo();
       
       if (saldoTotal >= valorSaque)
       {
@@ -36,16 +36,16 @@ public class ContaCorrente extends Conta {
          if (restante < 0)
          {
             setSaldo(0);
-            limiteAnterior = this.limite - restante;
+            limiteAnterior = this.limiteAtual - restante;
             
-            this.limite += restante;
+            this.limiteAtual += restante;
             return "Saque efetuado com sucesso! Seu saldo é de: R$ " + this.getSaldo() + ". "
-                    + "Seu limite é de: R$ " + this.limite + "! Houve uma movimentação de: R$ " + (limiteAnterior - limite);
+                    + "Seu limite é de: R$ " + this.limiteAtual + "! Houve uma movimentação de: R$ " + (limiteAnterior - limiteAtual);
          } else {
             setSaldo(getSaldo() - valorSaque);
             
             return "Saque efetuado com sucesso! Seu saldo é de: R$ " + getSaldo() + ". "
-                    + "Seu limite é de: R$ " + this.limite;
+                    + "Seu limite é de: R$ " + this.limiteAtual;
          }
       } else {
          return "Saldo indisponível!";
@@ -54,27 +54,27 @@ public class ContaCorrente extends Conta {
    
    public String depositar(double valorDeposito)
    {
-      if (limiteInicial > limite)
+      if (limiteInicial > limiteAtual)
       {
-         double restante = this.limiteInicial - this.limite;
+         double restante = this.limiteInicial - this.limiteAtual;
          
          if (valorDeposito > restante)
          {
-            this.limite = limiteInicial;
+            this.limiteAtual = limiteInicial;
             setSaldo(valorDeposito - restante);
             
             return "Depósito efetudado com sucesso! Seu novo saldo é: R$ " + getSaldo() + ". "
-                    + "Seu limite é de: R$ " + this.limite;
+                    + "Seu limite é de: R$ " + this.limiteAtual;
          } else {
-            this.limite += valorDeposito;
+            this.limiteAtual += valorDeposito;
             
             return "Depósito efetuado com sucesso! Seu novo saldo é: R$ " + getSaldo() + ". "
-                    + "Seu limite é de: R$ " + this.limite;
+                    + "Seu limite é de: R$ " + this.limiteAtual;
          }
       } else {
          setSaldo(getSaldo() + valorDeposito);
          return "Depósito efetuado com sucesso! Seu novo saldo é: R$ " + getSaldo() + ". "
-                    + "Seu limite é de: R$ " + this.limite;
+                    + "Seu limite é de: R$ " + this.limiteAtual;
       }
    }   
 }
